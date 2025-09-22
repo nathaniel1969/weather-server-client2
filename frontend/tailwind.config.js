c2812d6af37c21e42d84c804fe975884cbd25829
nathaniel001969 <nathaniel_r@hotmail.com>
2025-09-22 14:29:31 -0400

    Third commit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+});
